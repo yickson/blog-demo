b9ad48422c1dbd9bbab7c46ffe1941ef7887a82c
yickson <yicksonr@gmail.com>
2020-07-05 22:54:27 -0400

    Plantilla inicial para mi blog

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <img src="{{ asset('img/snack.png') }}" class="card-img-top" alt="snack">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+@endsection
